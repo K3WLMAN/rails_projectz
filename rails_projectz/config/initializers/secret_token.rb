d8bd2652ab6cd32602d14754c27cd8593a6bf749
coolman <cobbtech@gmail.com>
2014-03-07 22:21:55 -0600

    Initil comment

---
+++ rails_projectz/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsProjectz::Application.config.secret_key_base = '761027f5d74cf8083c86a8f505ac9646d6ed3a96dc6b956b05828523f5d51b064bf2207ee0f2e6bc29868badf07c225ae9204ded8a87ba425c3974f507326e3e'
---
+++ rails_projectz/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsProjectz::Application.config.session_store :cookie_store, key: '_rails_projectz_session'
